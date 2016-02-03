6118825f37cf2be56c97649ad4650780878de660
AnujaGhodekar <anuja_ghodekar@yahoo.in>
2016-02-03 13:45:33 +0530

    changes

--- var_fire.js
+++ var_fire.js
@@ -12,23 +12,21 @@ myFirebaseRef.child("Author").on("value", function(snapshot) {
 
 myFirebaseRef.child("DescWings").on("value", function(snapshot) {
   var s3 = snapshot.val();  
-  document.getElementById("demo3").innerHTML = s3;
-});
-
-
-myFirebaseRef.child("DesAPJ").on("value", function(snapshot) {
-  var s5 = snapshot.val();  
-document.getElementById("demo5").innerHTML = s5;
-});
-
-myFirebaseRef.child("DescIgnited").on("value", function(snapshot) {
-  var s6 = snapshot.val();  
-  document.getElementById("demo6").innerHTML = s6;
+  document.getElementById("demo").innerHTML = s3;
 });
 
 myFirebaseRef.child("DescIndia").on("value", function(snapshot) {
   var s7 = snapshot.val();  
- document.getElementById("demo7").innerHTML = s7;
+	document.getElementById("demo").innerHTML = s7;
 });
 
- 
+myFirebaseRef.child("DesAPJ").on("value", function(snapshot) {
+  var s5 = snapshot.val();  
+	document.getElementById("demo").innerHTML = s5;
+});
+
+myFirebaseRef.child("DescIgnited").on("value", function(snapshot) {
+  var s6 = snapshot.val();  
+	document.getElementById("demo").innerHTML = s6;
+});
+
