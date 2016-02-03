2816eea17a16b3626d34adb0e2e125f87e5b3ee4
AnujaGhodekar <anuja_ghodekar@yahoo.in>
2016-02-03 11:16:14 +0530

    changes

---
+++ var_fire.js
@@ -0,0 +1,34 @@
+var myFirebaseRef = new Firebase("https://booksinformation.firebaseio.com/");
+
+myFirebaseRef.child("Title").on("value", function(snapshot) {
+  var s1 = snapshot.val(); 
+  document.getElementById("d1").innerHTML = s1;
+});
+
+myFirebaseRef.child("Author").on("value", function(snapshot) {
+  var s2 = snapshot.val(); 
+  document.getElementById("d2").innerHTML = s2;
+});
+
+myFirebaseRef.child("DescWings").on("value", function(snapshot) {
+  var s3 = snapshot.val();  
+  document.getElementById("demo3").innerHTML = s3;
+});
+
+
+myFirebaseRef.child("DesAPJ").on("value", function(snapshot) {
+  var s5 = snapshot.val();  
+document.getElementById("demo5").innerHTML = s5;
+});
+
+myFirebaseRef.child("DescIgnited").on("value", function(snapshot) {
+  var s6 = snapshot.val();  
+  document.getElementById("demo6").innerHTML = s6;
+});
+
+myFirebaseRef.child("DescIndia").on("value", function(snapshot) {
+  var s7 = snapshot.val();  
+ document.getElementById("demo7").innerHTML = s7;
+});
+
+ 
